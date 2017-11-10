b7e1084e9701a61247c6b570605cfd4e2b331db3
John Wojciechowski <jwojo13@gmail.com>
2017-11-10 19:53:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,11 +11,9 @@ end
 
 
 def assign_rooms(array)
-  counter = 0
   welcome = []
   array.each {|name| welcome << "Hello, #{name}! You'll be assigned to room #{array.index(name)+1}!"}
   return welcome
-  counter += 1
 end
 
 
