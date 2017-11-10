5e6d21d1e072483349ad401fa9285de9770907cd
John Wojciechowski <jwojo13@gmail.com>
2017-11-10 19:43:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def batch_badge_creator(array)
 end
 
 
-def assign_rooms(name,array)
+def assign_rooms(array)
   welcome = []
   array.each {|room| welcome << "Hello, #{name}! You'll be assigned to #{array[room]-1}!"}
   return welcome
